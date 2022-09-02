231bb02205f9c450de653fe06743ddc7cab8ac7d
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-09-02 18:00:32 +0300

    add UserModel

---
+++ server/models/UserModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const emailValidator = require('email-validator');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    index: { unique: true },
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    index: { unique: true },
+    validate: { 
+      validator: emailValidator.validate,
+      message: props => `${props.value} is not a valid email address!`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    index: { unique: true },
+    minlength: 8
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema)
