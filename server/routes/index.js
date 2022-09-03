514e3b6a4c3b5210d955a66e284093a5e07fe599
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-09-03 17:05:45 +0300

    add user routes> registration, account and home

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const usersRoute = require('./users/index');
 
 router.get('/', (erq, res, next) => {
   return res.json({message: 'Home route'});
 });
+
+router.use('/users', usersRoute);
 
 module.exports = router;
 
--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { Controller } = require('./../users/controller')
 
-module.exports = () => {
-  router.get('/registeration', (req, res) => {
-    res.json({
-      message: 'success'
-    });
+router.get('/', (req, res) => {
+  res.json({
+    message: 'users home page'
   })
+});
 
-  router.post('/registeration', async(req, res, next) => {
-
+router.get('/registration', (req, res) => {
+  res.json({
+    message: 'success'
   });
+});
 
-  return router;
-};
+router.post('/registration', Controller.registration );
+
+router.get('/account', (req, res) => {
+  res.json({
+    message: 'user account'
+  });
+}); 
+
+module.exports = router;
