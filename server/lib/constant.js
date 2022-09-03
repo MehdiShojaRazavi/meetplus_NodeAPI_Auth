12b46851f867ee9b593de5c4f1484436d6e327e8
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-09-03 18:19:34 +0300

    add constants

---
+++ server/lib/constant.js
@@ -0,0 +1,8 @@
+const HttpStatus = {
+  OK: { code: 200, status: 'OK' },
+  CREATED: { code: 201, status: 'CREATED' },
+  NO_CONTENT: { code: 204, status: 'NO_CONTENT' },
+  BAD_REQUEST: { code: 400, status: 'OK' },
+  NOT_FOUND: { code: 404, status: 'NOT_FOUND' },
+  INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' },
+};
